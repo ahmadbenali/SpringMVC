a642484e58ca80012a5ca74fe3cc6252a3f521ae
“ahmadbenali” <a7madbenali@gmail.com>
2026-05-16 15:34:19 +0300

    small changes on controllers

--- src/main/java/com/Thymeleaf/demoThymeleaf/Controller/WelcomeController.java
+++ src/main/java/com/Thymeleaf/demoThymeleaf/Controller/WelcomeController.java
@@ -1,16 +1,19 @@
 package com.Thymeleaf.demoThymeleaf.Controller;
 
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class WelcomeController {
 
 
-    @RequestMapping("/showform")
+    @GetMapping("/showform")
     public String showForm() {
         return "Welcome_form";
     }
@@ -18,6 +21,19 @@ public class WelcomeController {
     @RequestMapping("/processForm")
     public String processForm(Model model) {
         model.addAttribute("TheDate",java.time.LocalDateTime.now());
+        return "HelloWorld";
+    }
+
+    @PostMapping("/processFormV2")
+    public String processFormV2(@RequestParam("studentName") String name, Model model) {
+        //read the request parameter from the html form
+
+        name = name.toUpperCase();
+
+        String Themessage="good? "+name;
+
+        model.addAttribute("message",Themessage);
+
         return "HelloWorld";
     }
 }
